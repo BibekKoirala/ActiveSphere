8e1933326e2cd64866da7a9e428071736097995e
Bibek Koirala <kbibek371@gmail.com>
2024-10-19 11:29:21 -0400

    cookie name changed in prod

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,33 @@
-import { getToken } from "next-auth/jwt"
-import { NextRequest, NextResponse } from "next/server"
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function middleware (req: NextRequest) {
+export async function middleware(req: NextRequest) {
   // If you don't have NEXTAUTH_SECRET set, you will have to pass your secret as `secret` to `getToken`
-  console.log(req)
-  const token = await getToken({ req , secret: "cPZyRbRKx0jRqLB+5STfoW+JgvzHi+/m3ajVaen6M/U="})
-  console.log(token)
+  console.log(req);
+  const token = await getToken({
+    req,
+    secret: "cPZyRbRKx0jRqLB+5STfoW+JgvzHi+/m3ajVaen6M/U=",
+    cookieName:
+      process.env.VERCEL_ENV === "development"
+        ? "next-auth.session-token"
+        : "__Secure-next-auth.session-token",
+  });
+  console.log(token);
   if (token) {
     // Signed in - Forward the token to the API by adding it to the request headers
     const response = NextResponse.next();
-    response.headers.set('Authorization', `${JSON.stringify(token)}`); // Set the token in the Authorization header
-    
+    response.headers.set("Authorization", `${JSON.stringify(token)}`); // Set the token in the Authorization header
+
     return response;
   } else {
     // Not Signed in - Return a 401 Unauthorized response
-    return NextResponse.json({ success: false, message: 'Unauthorized' }, { status: 401 });
+    return NextResponse.json(
+      { success: false, message: "Unauthorized" },
+      { status: 401 }
+    );
   }
 }
 
 export const config = {
-  matcher: ['/dashboard', '/api/posts', '/create-post', ],
-}
-
+  matcher: ["/dashboard", "/api/posts", "/create-post"],
+};
